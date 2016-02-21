96045d831224d3015dd45d6a598e50e3b3224ade
robjmberry <robjmberry@no-reply.com>
2016-02-21 18:34:32 +0000

    Added bootstrap for react.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import VisibleServiceRegistryList from './VisibleServiceRegistryList.jsx'
+import {Navbar} from 'react-bootstrap'
 
 const App = () =>(
+  <div>
+  <Navbar inverse>
+    <Navbar.Header>
+      <Navbar.Brand>Demo</Navbar.Brand>
+    </Navbar.Header>
+  </Navbar>
   <VisibleServiceRegistryList />
+  </div>
 )
 
 export default App;
--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ import conductorApp from './reducers'
 import App from './components/App.jsx';
 import {receiveRegistrations} from './actions'
 import data from './data/testdata'
+import 'bootstrap/dist/css/bootstrap.css'
 
 let store = createStore(conductorApp)
 store.dispatch(receiveRegistrations(data))
